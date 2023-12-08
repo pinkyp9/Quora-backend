15ce7292fadcad52172aa2d933ab4b9caa957951
pinky <thepinkypamecha@gmail.com>
2023-12-08 11:48:26 +0530

    new folder for database connection

--- server.js
+++ server.js
@@ -1,42 +1,39 @@
 import express from "express";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 import answerRoutes from "./routes/answerRoutes.js";
 import questionRoutes from "./routes/questionRoutes.js";
 import morgan from "morgan";
 import cors from "cors";
+import connectDb from "./config/dbConnection.js"
 dotenv.config();
+
+connectDb();
+
 const app = express();
 const port = process.env.PORT || 5000;
 const url = process.env.MONGODB_URI;
-app.use(morgan("combined"));
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("uploads",express.static('uploads'));
 app.use(cors());
-mongoose
-    .connect(url)
-    .then(() => {
-        console.log("Connected to mongoose");
-        app.listen(port, () => {
-            console.log(`Port is running at ${port}`);
-        });
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+app.use(morgan("combined"));
 
 app.use("/user", userRoutes);
 app.use("/answer", answerRoutes);
 app.use("/question", questionRoutes);
 
+app.use((err,req,res,next) => {
+    console.error({stackTrace:err.stack,message:err.message});
+    res.status(500).send("something went wrong!");
+})
 
 app.get("/",(req,res)=>{
     res.send("Welcome to your Q&A platform!");
 });
 
-app.use((err,req,res,net) => {
-    console.error(err.stack);
-    res.status(500).send("something went wrong!");
-})
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
+});
